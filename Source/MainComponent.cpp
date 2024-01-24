79052ae42fc9fefa1090f3dec4d78c2baaea9701
Zarif <zarifkarim9@gmail.com>
2024-01-24 14:39:08 -0500

    added borders to rectangle, started workoing on displayKeyMap.

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -11,10 +11,26 @@
 
 std::unordered_map<int, juce::Rectangle<int>> activeNotes;
 std::vector<juce::Rectangle<int>> prevNotes (120);
-std::set<int> whiteKeys = {2, 4, 5, 7, 9, 11, 12, 14, 16, 17, 19, 21, 23, 24, 26, 28, 29, 31, 33, 35, 36, 38, 40, 41, 43, 45, 47, 48, 50, 52, 53, 55, 57, 59, 60, 62, 64, 65, 67, 69, 71, 72, 74, 76, 77, 79, 81, 83, 84, 86, 88, 89, 91, 93, 95, 96, 98, 100, 101, 103, 105, 107, 108, 110, 112, 113, 115, 119, 120};
+std::set<int> whiteKeys = {0, 2, 4, 5, 7, 9, 11,
+    12, 14, 16, 17, 19, 21, 23,
+    24, 26, 28, 29, 31, 33, 35,
+    36, 38, 40, 41, 43, 45, 47,
+    48, 50, 52, 53, 55, 57, 59,
+    60, 62, 64, 65, 67, 69, 71,
+    72, 74, 76, 77, 79, 81, 83,
+    84, 86, 88, 89, 91, 93, 95,
+    96, 98, 100, 101, 103, 105, 107,
+    108, 110, 112, 113, 115, 119, 120};
+
 std::set<int> blackKeys = {1, 3, 6, 8, 10, 13, 15, 18, 20, 22, 25, 27, 30, 32, 34, 37, 39, 42, 44, 46, 49, 51, 54, 56, 58, 61, 63, 66, 68, 70, 73, 75, 78, 80, 82, 85, 87, 90, 92, 94, 97, 99, 102, 104, 106, 109, 111, 114, 116, 118};
-int PIXEL_MULTIPLIER = 7;
+
+std::unordered_map<int, juce::Rectangle<int>> displayKeyMap;
+
+juce::BorderSize<int> borderSize (10,10,10,10);
+
+int PIXEL_MULTIPLIER = 10;
 int NOTE_SPEED = 2;
+int KEY_HEIGHT = 50;
 juce::Image backgroundImage = juce::ImageFileFormat::loadFrom(juce::File("/Users/zarifkarim/Documents/MIDI_Viz/Builds/MacOSX/bkg.jpeg"));
 
 
@@ -40,6 +56,12 @@ MainComponent::MainComponent()
     //another possible idea is to have a button that lets you select if you have either 88 keys or 120 possible keys
     
     setSize (1000, 600);
+    
+    int numWhiteKeys = whiteKeys.size();
+    for (int i = 0; i < numWhiteKeys; i++) {
+        juce::Rectangle<int> key (i*PIXEL_MULTIPLIER, 0, PIXEL_MULTIPLIER, KEY_HEIGHT);
+        displayKeyMap.insert({i, key});
+    }
     
     setMidiInput();
     midiLabel.setText("midiText", juce::sendNotification);
@@ -84,17 +106,35 @@ void MainComponent::paint (juce::Graphics& g)
     // You can add your drawing code
     
     g.drawImageWithin(backgroundImage, 0, 0, Component::getWidth(), Component::getHeight(), juce::RectanglePlacement::fillDestination);
+    
     g.setColour(juce::Colours::orange);
     g.fillRect(10, 10, 40, 40);
+    g.setColour(juce::Colours::black);
+    g.drawRect(10, 10, 40, 40, 3);
     
+    
+    //display new notes
     for (auto& [id, note]: activeNotes) {
         g.setColour(juce::Colours::orange);
-        g.fillRect(note.getX() * PIXEL_MULTIPLIER , note.getY(), note.getWidth(), note.getHeight());
+        g.fillRect(note.getX(), note.getY(), note.getWidth(), note.getHeight());
+        g.setColour(juce::Colours::black);
+        g.drawRect(note.getX(), note.getY(), note.getWidth(), note.getHeight(), 1);
     }
     
+    //display old notes
     for (int i = 0; i < prevNotes.size(); i++){
         g.setColour(juce::Colours::orange);
-        g.fillRect(prevNotes.at(i).getX() * PIXEL_MULTIPLIER , prevNotes.at(i).getY() , prevNotes.at(i).getWidth(), prevNotes.at(i).getHeight());
+        g.fillRect(prevNotes.at(i).getX(), prevNotes.at(i).getY() , prevNotes.at(i).getWidth(), prevNotes.at(i).getHeight());
+        g.setColour(juce::Colours::black);
+        g.drawRect(prevNotes.at(i).getX(), prevNotes.at(i).getY() , prevNotes.at(i).getWidth(), prevNotes.at(i).getHeight(), 1);
+    }
+    
+    //repaint displayKeys
+    for (auto& [id, note]: displayKeyMap) {
+        g.setColour(juce::Colours::blue);
+        g.fillRect(note.getX(), Component::getHeight() - KEY_HEIGHT, note.getWidth(), note.getHeight());
+        g.setColour(juce::Colours::black);
+        g.drawRect(note.getX(), Component::getHeight() - KEY_HEIGHT, note.getWidth(), note.getHeight(), 3);
     }
 }
 
@@ -125,10 +165,9 @@ void MainComponent::handleIncomingMidiMessage(juce::MidiInput*, const juce::Midi
     int noteID = message.getNoteNumber();
     int velocity = message.getVelocity();
     bool isBlackKey = blackKeys.count(noteID) == 1;
-    int noteWidth = (isBlackKey) ? 4 : 6;
-//    int xOffset = (isBlackKey) ? 1 : 0;
+    int noteWidth = (isBlackKey) ? 4 : PIXEL_MULTIPLIER;
     if (velocity > 0) {
-        juce::Rectangle<int> note (noteID, Component::getHeight(), noteWidth, 1);
+        juce::Rectangle<int> note (noteID*PIXEL_MULTIPLIER, Component::getHeight()-KEY_HEIGHT, noteWidth, 1);
         activeNotes.insert({noteID, note});
     } else {
         if (activeNotes.count(noteID)==1){
