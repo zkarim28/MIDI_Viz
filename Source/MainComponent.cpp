5da88279dac5dff947de01a98f4ecb7f20ed75f1
Zarif <zarifkarim9@gmail.com>
2024-01-27 15:59:12 -0500

    old notes not visiable on screen are now deleted from prevNotes to save memory. scaled velocity color insity to ensure notes are not completely black if pressed too lightly

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -6,43 +6,54 @@
 #include <vector>
 #include <set>
 
-//std::unordered_map<int, juce::Rectangle<int>> activeNotes;
+//a very strange fix to my code implement the prevNotes data was to always check if the size of the vecotr
+//is greater than 4.
+
+//is multiplied with the noteID to allow for horizontal separation of displayed notes x coordinates
+int PIXEL_MULTIPLIER = 15;
+
+//used in moveNotes. controls how many pixels the y coordinate changed upward every refresh cycle
+int NOTE_SPEED = 2;
+
+//used for keyboard key information inside of displayKeyMap.
+int KEY_HEIGHT = 85;
+
+//constant num of MIDI signal possible.. from my keyboard at least
+int NUM_KEYS = 121;
+
+juce::Image backgroundImage = juce::ImageFileFormat::loadFrom(juce::File("/Users/zarifkarim/Documents/MIDI_Viz/Builds/MacOSX/bkg.jpeg"));
+
+//activNotes tracks all currently held down keys. Since there is only one possibility of each note,
+//it is easy to access each note separately via a hashmap
+//data layout: {..., {,{}} , {noteID, {x, y, width, height, isBlack, velocity}} , {,{}} , ...}
 std::unordered_map<int ,std::vector<int>> activeNotes;
-//{noteID, {x, y, width, height, isBlack, velocity}}
 
-//std::vector<juce::Rectangle<int>> prevNotes (120);
-std::vector<std::vector<int>> prevNotes (120);
+//prevNotes tracks all previously played notes, preserving all note information from activeNotes.
+//Since there can be multiple previous notes played from the same key, having a nested vector list allows for duplicates of noteIDs
+//yet with differing note information
+//data layout: {..., {}, {noteID, x, y, width, height, isBlack, velocity}, {}, ...}
+std::vector<std::vector<int>> prevNotes;
 
-//std::list<std::vector> prevNotes;
-//std::list<std::list<int>>({{}});
-
-
-//{x, y, width, height, isBlack, velocity}
-
-std::set<int> whiteKeys = {0, 2, 4, 5, 7, 9, 11,
-    12, 14, 16, 17, 19, 21, 23,
-    24, 26, 28, 29, 31, 33, 35,
-    36, 38, 40, 41, 43, 45, 47,
-    48, 50, 52, 53, 55, 57, 59,
-    60, 62, 64, 65, 67, 69, 71,
-    72, 74, 76, 77, 79, 81, 83,
-    84, 86, 88, 89, 91, 93, 95,
-    96, 98, 100, 101, 103, 105, 107,
-    108, 110, 112, 113, 115, 119, 120};
-
-std::set<int> blackKeys = {1, 3, 6, 8, 10, 13, 15, 18, 20, 22, 25, 27, 30, 32, 34, 37, 39, 42, 44, 46, 49, 51, 54, 56, 58, 61, 63, 66, 68, 70, 73, 75, 78, 80, 82, 85, 87, 90, 92, 94, 97, 99, 102, 104, 106, 109, 111, 114, 116, 118};
-
+//displayKeyMap represents the keys on the keyboard. keys on screen change color accoring to what key is played
+//and return to origianl color when not played.
+//Since there is only one of each key, it is easier to access the key information through a hashmap
 std::unordered_map<int, std::vector<int>> displayKeyMap;
 //{x, y, width, height, isOn}
 
-juce::BorderSize<int> borderSize (10,10,10,10);
+std::set<int> whiteKeys = 
+    {0, 2, 4, 5, 7, 9, 11,        //C -2    0<noteID<12
+    12, 14, 16, 17, 19, 21, 23,   //C -1    <24
+    24, 26, 28, 29, 31, 33, 35,   //C 0     <36
+    36, 38, 40, 41, 43, 45, 47,   //C 1     <48
+    48, 50, 52, 53, 55, 57, 59,   //C 2     <60
+    60, 62, 64, 65, 67, 69, 71,   //C 3     <72
+    72, 74, 76, 77, 79, 81, 83,   //C 4     <84
+    84, 86, 88, 89, 91, 93, 95,   //C 5     <96
+    96, 98, 100, 101, 103, 105, 107,  //C 6   <108
+    108, 110, 112, 113, 115, 119, 120}; // C7 < 120
 
-int PIXEL_MULTIPLIER = 15;
-int NOTE_SPEED = 2;
-int KEY_HEIGHT = 85;
-int NUM_KEYS = 121;
-juce::Image backgroundImage = juce::ImageFileFormat::loadFrom(juce::File("/Users/zarifkarim/Documents/MIDI_Viz/Builds/MacOSX/bkg.jpeg"));
-
+//list of all MIDI NoteIDs that are linked to the black keys on any MIDI keyboard
+std::set<int> blackKeys = {1, 3, 6, 8, 10, 13, 15, 18, 20, 22, 25, 27, 30, 32, 34, 37, 39, 42, 44, 46, 49, 51, 54, 56, 58, 61, 63, 66, 68, 70, 73, 75, 78, 80, 82, 85, 87, 90, 92, 94, 97, 99, 102, 104, 106, 109, 111, 114, 116, 118};
 
 //==============================================================================
 MainComponent::MainComponent()
@@ -76,6 +87,7 @@ MainComponent::MainComponent()
     setMidiInput();
     midiLabel.setText("midiText", juce::sendNotification);
     addAndMakeVisible(midiLabel);
+    
     Timer::startTimerHz(120);
 }
 
@@ -89,9 +101,7 @@ MainComponent::~MainComponent()
 
 //==============================================================================
 void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
-{
-    
-}
+{}
 
 void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill)
 {
@@ -99,9 +109,7 @@ void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& buffe
 }
 
 void MainComponent::releaseResources()
-{
-
-}
+{}
 
 //==============================================================================
 void MainComponent::paint (juce::Graphics& g)
@@ -113,19 +121,16 @@ void MainComponent::paint (juce::Graphics& g)
      y coordinate.
      */
     
-    // You can add your drawing code
-    
+    //draw background image
     g.drawImageWithin(backgroundImage, 0, 0, Component::getWidth(), Component::getHeight(), juce::RectanglePlacement::fillDestination);
     
-    g.setColour(juce::Colours::orange);
-    g.fillRect(10, 10, 40, 40);
-    g.setColour(juce::Colours::black);
-    g.drawRect(10, 10, 40, 40, 3);
+    int colorMultiplier = 0;
     
     //display new notes
     for (auto& [id, note]: activeNotes) {
-//        g.setColour(juce::Colours::orange);
-        g.setColour(juce::Colour(0, note.back()*2, note.back()*2));
+        colorMultiplier = (note.back() < 10) ? 30 : note.back();
+//        g.setColour(juce::Colour(0, note.back()*2, note.back()*2));
+        g.setColour(juce::Colour(0, colorMultiplier*2, colorMultiplier*2));
 //        g.fillRect(note.getX(), note.getY(), note.getWidth(), note.getHeight());
         g.fillRect(note[0], note[1], note[2], note[3]);
         g.setColour(juce::Colours::black);
@@ -133,11 +138,12 @@ void MainComponent::paint (juce::Graphics& g)
         g.drawRect(note[0], note[1], note[2], note[3], 1);
     }
 
-    
     //display old notes
     for(std::vector<int>& note: prevNotes){
         if(note.size() >= 4){
-            g.setColour(juce::Colour(0, note.back()*2, note.back()*2));
+//            g.setColour(juce::Colour(0, note.back()*2, note.back()*2));
+            colorMultiplier = (note.back() < 10) ? 30 : note.back();
+            g.setColour(juce::Colour(0, colorMultiplier*2, colorMultiplier*2));
             g.fillRect(note[0], note[1], note[2], note[3]);
             g.setColour(juce::Colours::black);
             g.drawRect(note[0], note[1], note[2], note[3], 1);
@@ -170,9 +176,6 @@ void MainComponent::resized()
 
 void MainComponent::handleIncomingMidiMessage(juce::MidiInput*, const juce::MidiMessage& message)
 {
-    DBG("Received MIDI message: " << message.getDescription());
-    
-    //display label test
     juce::String midiText;
     if (message.isNoteOnOrOff()) {
         midiText << "NoteOn: Channel " << message.getChannel();
@@ -186,15 +189,12 @@ void MainComponent::handleIncomingMidiMessage(juce::MidiInput*, const juce::Midi
     bool isBlackKey = blackKeys.count(noteID) != 0;
     int noteWidth = PIXEL_MULTIPLIER;
     if (velocity > 0) {
-//        juce::Rectangle<int> note (noteID*PIXEL_MULTIPLIER, Component::getHeight()-KEY_HEIGHT, noteWidth, 1);
         std::vector<int> note = {noteID*PIXEL_MULTIPLIER, Component::getHeight()-KEY_HEIGHT, noteWidth, 1, isBlackKey, velocity};
         displayKeyMap[noteID].back() = 1;
         activeNotes.insert({noteID, note});
     } else {
         if (activeNotes.count(noteID)==1){
-//            juce::Rectangle<int> oldNote (activeNotes[noteID][0], activeNotes[noteID][1], activeNotes[noteID][2], activeNotes[noteID][3]);
-            prevNotes.push_back(activeNotes[noteID]);    //change later when prevNote becomes hashmap
-//            prevNotes.push_back(oldNote);
+            prevNotes.push_back(activeNotes[noteID]);
             displayKeyMap[noteID].back() = 0;
             activeNotes.erase(noteID);
         }
@@ -211,32 +211,24 @@ void MainComponent::setMidiInput()
         deviceManager.setMidiInputDeviceEnabled (newInput.identifier, true);
 
     deviceManager.addMidiInputDeviceCallback (newInput.identifier, this);
-
 }
 
 void MainComponent::moveNotes()
 {
     for (auto& [noteID, note] : activeNotes){
-//        juce::Rectangle<int> updatedNote (activeNotes[noteID].getX(), activeNotes[noteID].getY()-NOTE_SPEED, activeNotes[noteID].getWidth(), activeNotes[noteID].getHeight()+NOTE_SPEED);
         std::vector<int> updatedNote = {note.at(0), note.at(1)-NOTE_SPEED, note.at(2), note.at(3)+NOTE_SPEED, note.at(4), note.at(5)};
         activeNotes[noteID] = updatedNote;
     }
     
     for(size_t i = 0; i < prevNotes.size(); i++){
         std::vector<int>& note = prevNotes[i];
-//        if (prevNotes.at(i).getY() + prevNotes.at(i).getHeight() < 0){
-//            prevNotes.erase(prevNotes.begin()+i);
-//            i--;
-//        }
+        if (note.size() >= 4 && note[1] + note[3] < 0){
+            prevNotes.erase(prevNotes.begin()+i);
+        }
         if(note.size() >= 4){
             std::vector<int> updatedNote = {note[0], note[1]-NOTE_SPEED, note[2], note[3], note[4], note[5]};
             prevNotes[i] = updatedNote;
         }
-//        juce::Rectangle<int> updatedNote (prevNotes.at(i).getX(), prevNotes.at(i).getY()-NOTE_SPEED, prevNotes.at(i).getWidth(), prevNotes.at(i).getHeight());
-//        std::vector<int> updatedNote = {prevNotes[i].at(0), prevNotes[i].at(1)-NOTE_SPEED, prevNotes[i].at(2), prevNotes[i].at(3)+NOTE_SPEED, prevNotes[i].at(4), prevNotes[i].at(5)};
-        
-        
-//        prevNotes[i] = updatedNote;
     }
 }
 
