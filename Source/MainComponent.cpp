407fb9bfb2211220f40ab70ab53e26603b01bc1e
Zarif <zarifkarim9@gmail.com>
2024-01-29 22:34:33 -0500

    read readme.

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -27,6 +27,28 @@ int KEY_HEIGHT = 100;
 //constant num of MIDI signal possible.. from my keyboard at least
 int NUM_KEYS = 121;
 
+//juce::File currentWorkingDirectory = juce::File::getCurrentWorkingDirectory();
+//juce::File imagePath = currentWorkingDirectory.getChildFile("Builds/MacOSX/bkg.jpeg"); // Relative path from the current working directory
+
+/*THERE WAS A STRANGE FACTORING OF MY FILES WHEN I ADDED THE BKG IMG
+
+ NOTICE:
+ 
+ 
+
+BEFORE RUNNING THE PROGRAM, PLEASE EDIT THE backgroundImage directory
+ string to the bkg.jpeg absolute path that works on your computer.
+ otherwise the program will not be able to run
+
+ juce::Image backgroundImage = juce::ImageFileFormat::loadFrom(juce::File("/...absolutePath/...bkg.jpeg"));
+ 
+ 1. find bkg somwhere in this folder, on xcode is can also be found in directly
+ 2. in xcode right click and click "Show File Inspector" or if not
+in xcode, you can also find it in your folders anyway
+ 3. copy full path in and paste into the double quotes found within the juce::File() constructor inside of the loadFrom function.
+
+*/
+
 juce::Image backgroundImage = juce::ImageFileFormat::loadFrom(juce::File("/Users/zarifkarim/Documents/MIDI_Viz/Builds/MacOSX/bkg.jpeg"));
 
 //activNotes tracks all currently held down keys. Since there is only one possibility of each note,
